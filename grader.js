c818e78c5f052d23690575cd3f904b07bb8272ae
Jon Ingi <jii@skeyti.com>
2013-07-11 23:04:19 +0000

    final version

--- grader.js
+++ grader.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -19,6 +20,10 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioString = function(string) {
+    return cheerio.load(string);
+};
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
@@ -31,7 +36,21 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
     }
-    return out;
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+};
+
+var checkString = function(string, checksfile) {
+    console.log(string);
+    $ = cheerioString(string);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
 };
 
 var clone = function(fn) {
@@ -41,11 +60,24 @@ var clone = function(fn) {
 if(require.main == module){
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), "")
+	.option('-u, --url <url>', 'URL to index.html')
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson = "";
+    if(program.file != "") {
+	console.log("checking file");
+	checkHtmlFile(program.file, program.checks);
+    } else {
+	restler.get(program.url).on('complete', function(result) {
+	    if(result instanceof Error) {
+		console.log("Error: " + result.message);
+		process.exit(1);
+	    } else {
+		checkString(result, program.checks);
+	    }
+	});
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkUrl = checkUrl;
 }
